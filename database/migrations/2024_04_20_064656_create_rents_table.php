ed1584240ad6f5f90766e337f16a1237d44b21a1
ramarfx <ramagamiing33@gmail.com>
2024-04-20 14:04:09 +0700

    rent model and migration

---
+++ database/migrations/2024_04_20_064656_create_rents_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('tenant');
+            $table->string('no_car');
+            $table->date('date_borrow');
+            $table->date('date_return');
+            $table->string('total');
+            $table->enum('status', ['on process', 'returned']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rents');
+    }
+};
