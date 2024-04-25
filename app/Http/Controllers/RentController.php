ea8ff32fd20c95388b07a25f0c7b30cba21e47d4
ramarfx <ramagamiing33@gmail.com>
2024-04-25 14:42:02 +0700

    tes

--- app/Http/Controllers/RentController.php
+++ app/Http/Controllers/RentController.php
@@ -72,8 +72,6 @@ class RentController extends Controller
             return response()->json(['message' => 'rent not found'], 404);
         }
 
-        //test
-
         return response()->json($rent);
     }
 
